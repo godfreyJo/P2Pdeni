433a270fcfca966029852e29e08b9b7ea799d8ce
godfreyJo <godfreyoyugi@gmail.com>
2020-05-21 08:15:23 +0300

    working files

--- src/settings/local.py
+++ src/settings/local.py
@@ -78,11 +78,14 @@ WSGI_APPLICATION = 'daraja.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'daraja',
+        'USER': 'postgres',
+        'PASSWORD': 'gody12345',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
